c0be472834175374b089160bb966a793f08aff94
LAPTOP-6A1KA1HF\Anuwat.ch <anuwatchaoyai@gmail.com>
2020-08-12 22:42:11 +0700

    async get data from firebase

--- pages/admin/posts/index.vue
+++ pages/admin/posts/index.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
     <PostList isAdmin :posts="loadData" />
+    <b-button @click="postsAll">Load...</b-button>
   </div>
 </template>
 <script>
 import PostList from "@/components/posts/PostList";
+import axios from "axios";
 export default {
   layout: "coreLayout",
   components: {
@@ -12,51 +14,17 @@ export default {
   },
   data() {
     return {
-      loadData: [
-        {
-          id: "1",
-          title: "เที่ยวบ้านช้าง",
-          content:
-            "Some quick example text to build on the card title and make up the bulk of the card's content.",
-          image: "https://picsum.photos/600/300/?image=25",
-        },
-        {
-          id: "2",
-          title: "เที่ยวบ้านลิง",
-          content:
-            "Some quick example text to build on the card title and make up the bulk of the card's content.",
-          image: "https://picsum.photos/600/300/?image=35",
-        },
-        {
-          id: "3",
-          title: "เที่ยวบ้านหนู",
-          content:
-            "Some quick example text to build on the card title and make up the bulk of the card's content.",
-          image: "https://picsum.photos/600/300/?image=44",
-        },
-        {
-          id: "4",
-          title: "เที่ยวทะเล",
-          content:
-            "Some quick example text to build on the card title and make up the bulk of the card's content.",
-          image: "https://picsum.photos/600/300/?image=10",
-        },
-        {
-          id: "5",
-          title: "เที่ยวภูเขา",
-          content:
-            "Some quick example text to build on the card title and make up the bulk of the card's content.",
-          image: "https://picsum.photos/600/300/?image=23",
-        },
-        {
-          id: "6",
-          title: "เที่ยวน้ำตก",
-          content:
-            "Some quick example text to build on the card title and make up the bulk of the card's content.",
-          image: "https://picsum.photos/600/300/?image=55",
-        },
-      ],
+      loadData: [],
     };
+  },
+  methods: {
+    async postsAll() {
+      await axios
+        .get("https://nuxt-tutorail.firebaseio.com/posts.json")
+        .then((res) => {
+          console.log(res);
+        });
+    },
   },
 };
 </script>
